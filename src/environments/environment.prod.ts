313ea4359fa08601eef803fd8acb4db7f94fe88a
Alexandre Sitole <alexandresitole@gmail.com>
2025-05-13 09:56:11 +0200

    build(android): add Android project setup and configuration files
    This commit introduces the initial setup for the Android project, including configuration files, Gradle setup, and resource files. The changes include:
    - Added `.gitignore` files for Android and app directories.
    - Included Gradle wrapper files and properties.
    - Added Android manifest, resource files, and layouts.
    - Configured build scripts and dependencies for the Android app.
    - Added test files for unit and instrumentation testing.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  supabaseUrl: 'https://ywmmsjudizjoojufwejo.supabase.co',
+  supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl3bW1zanVkaXpqb29qdWZ3ZWpvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDY1MzUxMjcsImV4cCI6MjA2MjExMTEyN30._ihqLNXF8k7YPM82qtFnsDH80Sz5jHNaxNNhZpVC1hA'
 };
